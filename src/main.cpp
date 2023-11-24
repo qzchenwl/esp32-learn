131910d33ad11bc8587a7742f6dd43ad41992bcc
qzchenwl <qzchenwl@gmail.com>
2023-11-24 16:58:46 +0800

    play audio file using driver/dac.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,20 @@
-#include "Arduino.h"
+#include <Arduino.h>
+#include <driver/dac.h>
 #include "SoundData.h"
 
-const int speakerPin = 25; // GPIO25用于扬声器
-
 void setup() {
-    // 初始化GPIO25为PWM输出
-    ledcSetup(0, 16000, 8); // 使用通道0, 16 kHz PWM频率, 8位分辨率
-    ledcAttachPin(speakerPin, 0); // 将GPIO25连接到通道0
+    // 启用DAC通道
+    dac_output_enable(DAC_CHANNEL_1);
 }
 
 void loop() {
-    // 遍历并播放音频样本数组
+    // 遍历音频样本数组
     for (unsigned char i : sample) {
-        ledcWrite(0, i); // 直接将样本值写入PWM
-        delayMicroseconds(62); // 根据16kHz采样率等待
+        // 输出当前样本值到DAC
+        dac_output_voltage(DAC_CHANNEL_1, i);
+        // 根据音频采样率延时（例如，16000Hz采样率对应62.5微秒）
+        delayMicroseconds(62);
     }
-
-    // 可能需要在这里添加一些逻辑来重复播放或停止
+    // 在音频播放结束后，可以加入一段延时
+    delay(1000);
 }
