ef421db7c6c8202fc1ccac657ad3206057cae031
qzchenwl <qzchenwl@gmail.com>
2023-12-03 12:35:41 +0800

    esp32-cam flash

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include<Arduino.h>
+
+int flashPin = 4;
+
+void setup() {
+    pinMode(flashPin, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(flashPin, HIGH);
+    delay(1000);
+    digitalWrite(flashPin, LOW);
+    delay(1000);
+}
+
