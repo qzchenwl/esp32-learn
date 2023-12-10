4ed1f5f3a590c8aa7fc25288df48a74a5ae15037
qzchenwl <qzchenwl@gmail.com>
2023-12-10 18:06:28 +0800

    DC/CS引脚可以使用函数调用

--- src/my_screen.cpp
+++ src/my_screen.cpp
@@ -3,6 +3,14 @@
 
 TFT_eSPI tft = TFT_eSPI();
 
+static void write_dc(uint8_t val) {
+  digitalWrite(3, val);
+}
+
+static void write_cs(uint8_t val) {
+  digitalWrite(15, val);
+}
+
 static bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap){
   if ((y >= tft.height())) return false;
   tft.pushImage(x, y, w, h, bitmap);
@@ -10,13 +18,15 @@ static bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* b
 }
 
 void setup_my_screen() {
-  TJpgDec.setJpgScale(1);
-  TJpgDec.setCallback(tft_output);
+  pinMode(3, OUTPUT);
+  pinMode(15, OUTPUT);
 
-  tft.init(); // 初始化显示器
+  tft.init(write_dc, write_cs); // 初始化显示器
   tft.setSwapBytes(true);
   tft.setRotation(1); // 设置显示方向
   tft.fillScreen(TFT_BLACK); // 清屏
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.initDMA();
+
+  TJpgDec.setJpgScale(1);
+  TJpgDec.setCallback(tft_output);
 }
