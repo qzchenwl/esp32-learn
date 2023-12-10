ec5dc95e45aa1615d3070d900369de0d5c6de5d0
xucanjie <xu.canjie@gmail.com>
2023-12-10 18:26:34 +0800

    diycam

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,35 @@
 #include <Arduino.h>
 #include <esp_camera.h>
-#include <TJpg_Decoder.h>
-
 #include "my_camera.h"
-#include "my_screen.h"
+#include <WiFi.h>
 
+#include <HTTPClient.h>
+const char* serverName = "http://192.168.12.229:8000/upload";
+
+
+const char* ssid = "Xiaomi";
+const char* password = "87883222";
 void setup() {
   Serial.begin(115200);
   Serial.println("setup ...");
+  setup_my_camera();
+
+  WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+
 
   Serial.println("setup my screen ...");
-  setup_my_screen();
 
   Serial.println("setup my camera ...");
-  setup_my_camera();
 
   delay(2000);
 }
@@ -25,10 +41,13 @@ void loop() {
   t1 = millis() - t1;
 
   int t2 = millis();
-  TJpgDec.drawJpg(0, 10, fb->buf, fb->len);
   t2 = millis() - t2;
 
+  //Serial.println(String((char *)fb->buf, fb->len));
+
+
+  // 关闭 HTTP 连接
+
+  Serial.printf("t1=%dms t2=%dms l=%d\r\n", t1, t2, fb->len);
   esp_camera_fb_return(fb);
-  tft.setCursor(0, 0);
-  tft.printf("t1=%dms t2=%dms", t1, t2);
 }
